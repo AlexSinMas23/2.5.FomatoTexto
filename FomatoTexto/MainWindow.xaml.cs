467941712b9cf5e5119e31632e96fd4877ea9c4b
Alexander Mota <AlexanderMota@outlook.es>
2021-09-30 14:00:58 +0200

    para entragar

--- FomatoTexto/MainWindow.xaml.cs
+++ FomatoTexto/MainWindow.xaml.cs
@@ -9,15 +9,12 @@ namespace FomatoTexto
     public partial class MainWindow : Window
     {
         public MainWindow() => InitializeComponent();
-
         private void botonAzul_Click(object sender, RoutedEventArgs e)
             => textoTrim.Foreground= Brushes.Blue;
         private void botonRojo_Click(object sender, RoutedEventArgs e)
             => textoTrim.Foreground = Brushes.Red;
         private void botonVerde_Click(object sender, RoutedEventArgs e)
             => textoTrim.Foreground = Brushes.Green;
-        private void cajaFormaTexto_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
-            => textoTrim.Text = cajaFormaTexto.Text;
         private void CheckBoxNegrita_Checked(object sender, RoutedEventArgs e)
              => textoTrim.FontWeight = FontWeights.Bold;
         private void CheckBoxNegrita_Unchecked(object sender, RoutedEventArgs e)
@@ -26,6 +23,8 @@ namespace FomatoTexto
             => textoTrim.FontStyle = FontStyles.Italic;
         private void CheckBoxCursiva_Unchecked(object sender, RoutedEventArgs e)
             => textoTrim.FontStyle = FontStyles.Normal;
+        private void cajaFormaTexto_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+            => textoTrim.Text = cajaTexto.Text;
 
     }
 }
